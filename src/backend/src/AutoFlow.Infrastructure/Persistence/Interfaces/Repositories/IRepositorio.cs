b445d5e9a78cd085e9d9934f9efa7036931b7e0c
Ronaldo Carneiro da Silva Filho <kikogamer@gmail.com>
2026-08-02 11:22:53 -0300

    criando a base de implementação do repositório

---
+++ src/backend/src/AutoFlow.Infrastructure/Persistence/Interfaces/Repositories/IRepositorio.cs
@@ -0,0 +1,13 @@
+﻿using AutoFlow.Domain.Models;
+
+namespace AutoFlow.Infrastructure.Interfaces.Repositories
+{
+    public interface IRepositorio<TBaseModel> : IDisposable where TBaseModel : BaseModel
+    {
+        Task Adicionar(TBaseModel entidade);
+        Task<TBaseModel?> ObterPorId(int id);
+        Task<List<TBaseModel>> ObterTodos();
+        Task Atualizar(TBaseModel entidade);
+        Task Remover(int id);
+    }
+}
---
+++ src/backend/src/AutoFlow.Infrastructure/Persistence/Repositories/Repository.cs
@@ -0,0 +1,51 @@
+﻿using AutoFlow.Domain.Models;
+using AutoFlow.Infrastructure.Interfaces.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace AutoFlow.Infrastructure.Persistence.Repositories
+{
+    public abstract class Repositorio<TBaseModel> : IRepositorio<TBaseModel> where TBaseModel : BaseModel, new()
+    {
+        protected readonly AppDbContext Db;
+        protected readonly DbSet<TBaseModel> DbSet;
+
+        protected Repositorio(AppDbContext db)
+        {
+            Db = db;
+            DbSet = db.Set<TBaseModel>();
+        }
+
+        public Task Adicionar(TBaseModel entidade)
+        {
+            DbSet.Add(entidade);
+            return Db.SaveChangesAsync();
+        }
+
+        public Task Atualizar(TBaseModel entidade)
+        {
+            DbSet.Update(entidade);
+            return Db.SaveChangesAsync();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<TBaseModel?> ObterPorId(int id)
+        {
+            return await DbSet.FindAsync(id);
+        }
+
+        public async Task<List<TBaseModel>> ObterTodos()
+        {
+            return await DbSet.ToListAsync();
+        }
+
+        public async Task Remover(int id)
+        {
+            DbSet.Remove(new TBaseModel { Id = id });
+            await Db.SaveChangesAsync();
+        }
+    }
+}
