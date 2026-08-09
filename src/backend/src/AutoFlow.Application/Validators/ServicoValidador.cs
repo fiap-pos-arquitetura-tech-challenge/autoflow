6f3a2ad9b854957d15b628428a1a349d9cb42528
caiomamprin <caiomamprin7@gmail.com>
2026-08-09 17:49:44 -0300

    Add a classe Validador de Serviço

---
+++ src/backend/src/AutoFlow.Application/Validators/ServicoValidador.cs
@@ -0,0 +1,23 @@
+﻿using AutoFlow.Application.Interfaces.DTOs;
+using AutoFlow.Application.Services;
+using AutoFlow.Application.Services.Enums;
+
+namespace AutoFlow.Application.Validators
+{
+    public static class ServicoValidador
+    {
+        public static Result? Validar(IServicoDTO servico)
+        {
+            if (string.IsNullOrWhiteSpace(servico.Nome))
+                return Result.Failure("Nome é obrigatório.", ErrorType.Validation);
+
+            if (string.IsNullOrWhiteSpace(servico.Preco.ToString()))
+                return Result.Failure("Preço é obrigatório.", ErrorType.Validation);
+
+            if (string.IsNullOrWhiteSpace(servico.TempoMedio.ToString()))
+                return Result.Failure("Tempo médio é obrigatório.", ErrorType.Validation);
+
+            return null;
+        }
+    }
+}
