61e9b92267f1796f6a754ae310fd6323a9876104
caiomamprin <caiomamprin7@gmail.com>
2026-08-09 17:48:07 -0300

    Add ServicoDTO

---
+++ src/backend/src/AutoFlow.Application/DTOs/ServicoDto.cs
@@ -0,0 +1,25 @@
+﻿using AutoFlow.Application.Interfaces.DTOs;
+
+namespace AutoFlow.Application.DTOs
+{
+    public record ServicoDto(
+        int Id,
+        string Nome,
+        decimal Preco,
+        int TempoMedio
+    );
+
+    public record CriaServicoDto(
+           int Id,
+           string Nome,
+           decimal Preco,
+           int TempoMedio
+       ) : IServicoDTO;
+
+    public record AtualizaServicoDto(
+           int Id,
+           string Nome,
+           decimal Preco,
+           int TempoMedio
+       ) : IServicoDTO;
+}
