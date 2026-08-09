04a30d9b36712e552bbeb7ffd407e470b078554d
caiomamprin <caiomamprin7@gmail.com>
2026-08-09 17:49:05 -0300

    Add a Interface da Service de Serviço

---
+++ src/backend/src/AutoFlow.Application/Interfaces/Services/IServicoService.cs
@@ -0,0 +1,14 @@
+﻿using AutoFlow.Application.DTOs;
+using AutoFlow.Application.Services;
+
+namespace AutoFlow.Application.Interfaces.Services
+{
+    public interface IServicoService
+    {
+        public Task<Result<ServicoDto>> AdicionarAsync(CriaServicoDto servicoDto);
+        public Task<Result<ServicoDto>> AtualizarAsync(int id, AtualizaServicoDto servicoDto);
+        public Task<Result> ExcluirAsync(int id);
+        public Task<Result<ServicoDto>> ObterPorIdAsync(int id);
+        public Task<IEnumerable<ServicoDto>> ObterTodosAsync();
+    }
+}
