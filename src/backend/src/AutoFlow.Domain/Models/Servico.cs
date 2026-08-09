414624f480c791db4d8f2a495d233d85855d83ff
caiomamprin <caiomamprin7@gmail.com>
2026-08-09 17:49:56 -0300

    Add a Model de Serviço

---
+++ src/backend/src/AutoFlow.Domain/Models/Servico.cs
@@ -0,0 +1,23 @@
+﻿namespace AutoFlow.Domain.Models
+{
+    public class Servico : BaseModel
+    {
+        public string Nome { get; set; }
+        public decimal Preco { get; set; }
+        public int TempoMedio { get; set; }
+
+        public Servico()
+        {
+            Nome = null!;
+            Preco = 0;
+            TempoMedio = 0;
+        }
+        public Servico(string nome, decimal preco, int tempoMedio)
+        {
+            Nome = nome;
+            Preco = preco;
+            TempoMedio = tempoMedio;
+        }
+    }
+}
+     
