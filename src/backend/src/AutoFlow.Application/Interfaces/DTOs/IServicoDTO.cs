b9ad351d584b69719452d4b2d8db7e9f87b148ce
caiomamprin <caiomamprin7@gmail.com>
2026-08-09 17:48:24 -0300

    Add a interface do ServicoDTO

---
+++ src/backend/src/AutoFlow.Application/Interfaces/DTOs/IServicoDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace AutoFlow.Application.Interfaces.DTOs
+{
+    public interface IServicoDTO
+    {
+        public int Id { get; }
+        public string Nome { get; }
+        public decimal Preco { get; }
+        public int TempoMedio { get; }
+
+    }
+}
