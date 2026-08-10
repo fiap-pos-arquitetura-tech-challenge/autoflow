2403e794f96279dbaef15459e76a84409370a522
caiomamprin <caiomamprin7@gmail.com>
2026-08-09 22:35:24 -0300

    Add o implementação do Repositorio de Servico

---
+++ src/backend/src/AutoFlow.Infrastructure/Persistence/Repositories/ServicoRepositorio.cs
@@ -0,0 +1,9 @@
+﻿using AutoFlow.Application.Interfaces.Repositories;
+using AutoFlow.Domain.Models;
+
+namespace AutoFlow.Infrastructure.Persistence.Repositories
+{
+    public class ServicoRepositorio(AppDbContext db) : Repositorio<Servico>(db), IServicoRepositorio
+    {
+    }
+}
