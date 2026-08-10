7a26b7eee47a126ce771756773c408d28311a2b4
caiomamprin <caiomamprin7@gmail.com>
2026-08-09 22:35:06 -0300

    Add o Mapeamento do Entity para o BD da tabela Servico

---
+++ src/backend/src/AutoFlow.Infrastructure/Persistence/Configurations/ServicoConfiguration.cs
@@ -0,0 +1,29 @@
+﻿using AutoFlow.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AutoFlow.Infrastructure.Persistence.Configurations
+{
+    public class ServicoConfiguration : IEntityTypeConfiguration<Servico>
+    {
+        public void Configure(EntityTypeBuilder<Servico> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Nome)
+                .IsRequired()
+                .HasColumnType("varchar(100)");
+
+            builder.Property(x => x.Preco)
+                .IsRequired()
+                .HasColumnType("decimal(10,2)");
+
+            builder.Property(x => x.TempoMedio)
+                .IsRequired()
+                .HasColumnType("int");
+
+            builder.ToTable("Servico");
+        }
+    }
+    
+}
