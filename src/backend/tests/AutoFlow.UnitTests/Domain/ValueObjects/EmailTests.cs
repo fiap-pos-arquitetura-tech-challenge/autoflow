94b036bbd83f513cd95e923652b1588608dac041
Ronaldo Carneiro da Silva Filho <kikogamer@gmail.com>
2026-08-12 22:29:14 -0300

    adicionando validação de formato de e-mail

--- src/backend/tests/AutoFlow.UnitTests/Domain/ValueObjects/EmailTests.cs
+++ src/backend/tests/AutoFlow.UnitTests/Domain/ValueObjects/EmailTests.cs
@@ -23,5 +23,31 @@ namespace AutoFlow.UnitTests.Domain.ValueObjects
 
             Assert.Equal("Email é obrigatório.", exception.Message);
         }
+
+        [Theory]
+        [InlineData("joao@email.com")]
+        [InlineData("joao.silva@email.com.br")]
+        [InlineData("joao+trabalho@sub.dominio.com")]
+        [InlineData("j@d.co")]
+        public void Construtor_ComEnderecoValido_NaoDeveLancarExcecao(string endereco)
+        {
+            var email = new Email(endereco);
+
+            Assert.Equal(endereco, email.Endereco);
+        }
+
+        [Theory]
+        [InlineData("email-sem-arroba.com")]
+        [InlineData("@email.com")]
+        [InlineData("joao@")]
+        [InlineData("joao@email")]
+        [InlineData("joao@email..com")]
+        [InlineData("joao email@email.com")]
+        public void Construtor_ComEnderecoInvalido_DeveLancarEmailInvalidoException(string endereco)
+        {
+            var exception = Assert.Throws<EmailInvalidoException>(() => new Email(endereco));
+
+            Assert.Equal("Email inválido.", exception.Message);
+        }
     }
 }
